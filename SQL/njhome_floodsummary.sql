3437d68c7b1fd9ad8dd0d63e8ceeb2395736e482
gaudiom4git <sailfunds29@gmail.com>
2022-06-22 15:05:45 -0400

    Dropped Qtrs Added Storm.Size,Elevation and PriceDrop

---
+++ SQL/njhome_floodsummary.sql
@@ -0,0 +1,10 @@
+insert into njhome_floodsummary
+select pcr.city, pcr.lat, pcr.long, pcr.elevation, pcr.month, pcr.year, 
+pcr.city_max_day_rain, pcr.city_month_total_rain, pcr.city_avg_daily_rain, 
+pcr.anomalypct, pcr.anomaly,
+njh.county, njh.sizerank,
+njh.date, njh.avg_home_price, njh.price_drop
+from per_city_rainfall pcr, njhomeprice njh
+where pcr.city = njh.city
+and pcr.month = njh.month
+and pcr.year = njh.year
