efe3631f04c59793d3b10c587f2a56216164c034
gaudiom4git <sailfunds29@gmail.com>
2022-06-26 09:46:38 -0400

    AWS added and final CSV

--- SQL/njhome_floodsummary.sql
+++ SQL/njhome_floodsummary.sql
@@ -1,9 +1,7 @@
-// Update city name to match rainfall 
-update njhomeprice
-set city = 'Bridgewater'
-where city = 'Bridgewater Township'
-
-// Create merge file 
+-- Clear njhome_floodsummary
+delete from njhome_floodsummary;
+-- Join rainfall and homeprice file to 
+-- Create summary file for ML and Tableau
 insert into njhome_floodsummary
 select pcr.city, pcr.lat, pcr.long, pcr.elevation, pcr.month, pcr.year, 
 pcr.city_max_day_rain, pcr.city_month_total_rain, pcr.city_avg_daily_rain, 
@@ -13,4 +11,4 @@ njh.date, njh.avg_home_price, njh.price_drop
 from per_city_rainfall pcr, njhomeprice njh
 where pcr.city = njh.city
 and pcr.month = njh.month
-and pcr.year = njh.year
+and pcr.year = njh.year;
